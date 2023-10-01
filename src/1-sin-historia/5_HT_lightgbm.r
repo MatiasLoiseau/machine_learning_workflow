919dcba2a5caf78b83449cc1fc8e1bb24f669fde
MatiasLoiseau <matiasloiseau@gmail.com>
2023-10-01 17:30:10 -0300

    update: cambiar nombres directorios y agregar semillas

--- src/1-sin-historia/5_HT_lightgbm.r
+++ src/1-sin-historia/5_HT_lightgbm.r
@@ -27,14 +27,14 @@ options(error = function() {
 
 # Parametros del script
 PARAM <- list()
-PARAM$experimento <- "HT6510"
+PARAM$experimento <- "HT6511"
 
-PARAM$exp_input <- "TS6410"
+PARAM$exp_input <- "TS6411"
 
 # En caso que se haga cross validation, se usa esta cantidad de folds
 PARAM$lgb_crossvalidation_folds <- 5
 
-PARAM$lgb_semilla <- 102191 # cambiar por su propia semilla
+PARAM$lgb_semilla <- 374063 # cambiar por su propia semilla
 
 
 # Hiperparametros FIJOS de  lightgbm
