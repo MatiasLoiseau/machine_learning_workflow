9de03ef9cfec0198197827a6c911e69cc8268ac0
MatiasLoiseau <matiasloiseau@gmail.com>
2023-10-01 23:34:17 -0300

    fix: path source workflow

--- src/0-workflow-inicial/z601_RUN_correr_workflow.r
+++ src/0-workflow-inicial/z601_RUN_correr_workflow.r
@@ -10,11 +10,11 @@ options(error = function() {
 # corrida de cada paso del workflow
 
 # primeros pasos, relativamente rapidos
-source("~/machine_learning_workflow/src/workflow-inicial/z611_CA_reparar_dataset.r")
-source("~/machine_learning_workflow/src/workflow-inicial/z621_DR_corregir_drifting.r")
-source("~/machine_learning_workflow/src/workflow-inicial/z631_FE_historia.r")
-source("~/machine_learning_workflow/src/workflow-inicial/z641_TS_training_strategy.r")
+source("~/machine_learning_workflow/src/0-workflow-inicial/z611_CA_reparar_dataset.r")
+source("~/machine_learning_workflow/src/0-workflow-inicial/z621_DR_corregir_drifting.r")
+source("~/machine_learning_workflow/src/0-workflow-inicial/z631_FE_historia.r")
+source("~/machine_learning_workflow/src/0-workflow-inicial/z641_TS_training_strategy.r")
 
 # ultimos pasos, muy lentos
-source("~/machine_learning_workflow/src/workflow-inicial/z651_HT_lightgbm.r")
-source("~/machine_learning_workflow/src/workflow-inicial/z661_ZZ_final.r")
+source("~/machine_learning_workflow/src/0-workflow-inicial/z651_HT_lightgbm.r")
+source("~/machine_learning_workflow/src/0-workflow-inicial/z661_ZZ_final.r")
