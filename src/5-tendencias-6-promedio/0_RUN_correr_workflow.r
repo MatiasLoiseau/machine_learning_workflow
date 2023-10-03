64d2365dea58d019816166fa7796aa1916f538f0
MatiasLoiseau <matiasloiseau@gmail.com>
2023-10-03 17:44:44 -0300

    fix: path RUN worflow experimento 5

--- src/5-tendencias-6-promedio/0_RUN_correr_workflow.r
+++ src/5-tendencias-6-promedio/0_RUN_correr_workflow.r
@@ -10,9 +10,9 @@ options(error = function() {
 # corrida de cada paso del workflow
 
 # primeros pasos, relativamente rapidos
-source("~/machine_learning_workflow/src/6-tendencias-6-ratioavg/1_CA_reparar_dataset.r")
-source("~/machine_learning_workflow/src/6-tendencias-6-ratioavg/2_DR_corregir_drifting.r")
-source("~/machine_learning_workflow/src/6-tendencias-6-ratioavg/3_FE_historia.r")
-source("~/machine_learning_workflow/src/6-tendencias-6-ratioavg/4_TS_training_strategy.r")
-source("~/machine_learning_workflow/src/6-tendencias-6-ratioavg/5_HT_lightgbm.r")
-source("~/machine_learning_workflow/src/6-tendencias-6-ratioavg/6_ZZ_final.r")
+source("~/machine_learning_workflow/src/5-tendencias-6-promedio/1_CA_reparar_dataset.r")
+source("~/machine_learning_workflow/src/5-tendencias-6-promedio/2_DR_corregir_drifting.r")
+source("~/machine_learning_workflow/src/5-tendencias-6-promedio/3_FE_historia.r")
+source("~/machine_learning_workflow/src/5-tendencias-6-promedio/4_TS_training_strategy.r")
+source("~/machine_learning_workflow/src/5-tendencias-6-promedio/5_HT_lightgbm.r")
+source("~/machine_learning_workflow/src/5-tendencias-6-promedio/6_ZZ_final.r")
