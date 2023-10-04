40528718f1a3c894b8c5197002cc86c673d38ce2
MatiasLoiseau <matiasloiseau@gmail.com>
2023-10-04 18:52:13 -0300

    fix: path

--- src/10-sin-tendencia/0_RUN_correr_workflow.r
+++ src/10-sin-tendencia/0_RUN_correr_workflow.r
@@ -10,9 +10,9 @@ options(error = function() {
 # corrida de cada paso del workflow
 
 # primeros pasos, relativamente rapidos
-source("~/machine_learning_workflow/src/1-sin-tendencia/1_CA_reparar_dataset.r")
-source("~/machine_learning_workflow/src/1-sin-tendencia/2_DR_corregir_drifting.r")
-source("~/machine_learning_workflow/src/1-sin-tendencia/3_FE_historia.r")
-source("~/machine_learning_workflow/src/1-sin-tendencia/4_TS_training_strategy.r")
-source("~/machine_learning_workflow/src/1-sin-tendencia/5_HT_lightgbm.r")
-source("~/machine_learning_workflow/src/1-sin-tendencia/6_ZZ_final.r")
+source("~/machine_learning_workflow/src/10-sin-tendencia/1_CA_reparar_dataset.r")
+source("~/machine_learning_workflow/src/10-sin-tendencia/2_DR_corregir_drifting.r")
+source("~/machine_learning_workflow/src/10-sin-tendencia/3_FE_historia.r")
+source("~/machine_learning_workflow/src/10-sin-tendencia/4_TS_training_strategy.r")
+source("~/machine_learning_workflow/src/10-sin-tendencia/5_HT_lightgbm.r")
+source("~/machine_learning_workflow/src/10-sin-tendencia/6_ZZ_final.r")
